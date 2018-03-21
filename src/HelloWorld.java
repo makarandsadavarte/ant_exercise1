df0c71c9985d5d7f2ac2514bc80a28b301d385b9
Makarand Sadavarte <msadavarte@xtvia.com>
2018-03-21 13:26:35 +0530

    addin all files

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World! from Makarand" );
+        System.exit( 0 ); //success
+    }
+}
---
+++ src/HelloWorld1.java
@@ -0,0 +1,6 @@
+public class HelloWorld1 {
+    public static void main( String[] args ) {
+        System.out.println( "Welcome to world of ANT" );
+        System.exit( 0 ); //success
+    }
+}
