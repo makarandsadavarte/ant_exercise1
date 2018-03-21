8584c27571daf8b4638e532a94e49b9aaef75394
Makarand Sadavarte <msadavarte@xtvia.com>
2018-03-21 19:43:45 +0530

    Print message modified in the  java file

--- src/HelloWorld1.java
+++ src/HelloWorld1.java
@@ -1,6 +1,6 @@
 public class HelloWorld1 {
     public static void main( String[] args ) {
-        System.out.println( "Welcome to world of ANT" );
+        System.out.println( "Hi This is my first ANT job configured in Jenkins using Github" );
         System.exit( 0 ); //success
     }
 }
